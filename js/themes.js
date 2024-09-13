7252483ab4b65fef82eb0389b3a3e5d78e56d3e5
Animesh Mondal <animeshtxt@kgpian.iitkgp.ac.in>
2024-09-13 22:30:10 +0530

    update file name from Thems to themes

---
+++ js/themes.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.Slider' , {
+    autoplay:true,
+    speed:2500,
+    loop:true,
+    parallax:true,
+
+    navigation:{
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+    },
+});
