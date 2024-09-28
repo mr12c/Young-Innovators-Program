bb6a5803f1ca46228ab8cefd729128e0da59bad8
dhruv1412 <dhruvsailorsail@gmail.com>
2024-09-29 00:52:56 +0530

    Update : Slide Show of Themes

--- js/themes.js
+++ js/themes.js
@@ -1,11 +1,40 @@
-var swiper = new Swiper('.Slider' , {
-    autoplay:true,
-    speed:2500,
-    loop:true,
-    parallax:true,
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+const leftArrow = document.querySelector(".controls i:nth-child(1)");
+const rightArrow = document.querySelector(".controls i:nth-child(2)");
+const dotsWrapper = document.querySelector(".dots");
+const dots = document.querySelectorAll(".dots span");
 
-    navigation:{
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next',
-    },
+let slideIndex = 0;
+
+const setIndex = () => {
+  document.querySelector(".dots .active").classList.remove("active");
+  slider.style.transform = `translateX(${slideIndex * -100}%)`;
+  // timer.stop();
+  // timer.start();
+};
+
+dots.forEach((dot, ind) => {
+  dot.addEventListener("click", () => {
+    slideIndex = ind;
+    setIndex();
+    dot.classList.add("active");
+  });
 });
+  
+leftArrow.addEventListener("click", () => {
+  slideIndex = slideIndex > 0 ? slideIndex - 1 : slides.length - 1 ;
+  setIndex();
+  dotsWrapper.children[slideIndex].classList.add("active");
+});
+
+rightArrow.addEventListener("click", () => {
+  slideIndex =
+    slideIndex < slides.length - 1 ? slideIndex + 1 : 0;
+  setIndex();
+  dotsWrapper.children[slideIndex].classList.add("active");
+});
+
+setInterval(() => {
+    rightArrow.click();
+}, 7000);
